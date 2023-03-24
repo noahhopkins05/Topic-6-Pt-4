bd9d94ea6d24003d75b098d52824fbdd2fec8e5a
Noah Hopkins <noah.mich.hopk@gmail.com>
2023-03-24 10:02:04 -0400

    Add project files.

---
+++ Topic 6 Pt 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Topic_6_Pt_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNum, maxNum, randomNum;
+            int i = 0;
+            Random generator = new Random();
+
+            while (true)
+            {
+                Console.WriteLine("Enter the minimum number:");
+                minNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the maximum number");
+                maxNum = Int32.Parse(Console.ReadLine());
+
+                if (minNum > maxNum)
+                {
+                    Console.WriteLine("Please try again.");
+                }
+
+                else
+                {
+                    while (i < 25)
+                    {
+                        randomNum = generator.Next(minNum, maxNum + 1);
+                        Console.Write(randomNum + " ");
+                        i++;
+
+                    }
+                    break;
+                }
+            }
+            
+
+            
+        }
+    }
+}
